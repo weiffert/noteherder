ae72d6f8655e9e883ffd3c664c1968866d106864
weiffert <weiffert@gmail.com>
2018-05-22 11:58:54 -0400

    Begin styling sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,9 +6,9 @@ import newHover from "./new-hover.png";
 
 const Sidebar = () => {
   return (
-    <div className="Sidebar">
-      <div className="logo">
-        <img src={quill} alt="Noteherder" />
+    <div className="Sidebar" style={styles.sidebar}>
+      <div className="logo" style={styles.logo}>
+        <img src={quill} alt="Noteherder" style={styles.logoImg} />
       </div>
 
       <a href="/notes">
@@ -25,6 +25,57 @@ const Sidebar = () => {
   );
 };
 
-const style = {};
+const styles = {
+
+sidebar: {
+  width: "6rem",
+  backgroundColor: "#f3f3f3",
+  padding: "0.5rem 0", 
+  display: "flex", 
+  flexDirection: "column",
+  alignItems: "center",
+},
+logo: {
+  fontFamily: "Fauna One",
+  color: "#666",
+  fontSize: "3rem",
+},
+logoImg: {
+  width: "3rem",
+  paddingLeft: "0.5rem",
+},
+
+// .Sidebar a.new-note {
+//   margin-top: 2rem;
+//   position: relative;
+//   width: 40px;
+// }
+// .Sidebar button {
+//   background-color: transparent;
+//   border: 0;
+//   color: #008BF8;
+//   cursor: pointer;
+// }
+// .Sidebar a img {
+//   position: absolute;
+//   left: 0;
+//   width: 100%;
+//   transition: opacity 0.25s ease-in-out;
+// }
+// .Sidebar a:hover img.outline {
+//   opacity: 0;
+// }
+
+// .Sidebar .SignOut {
+//   position: absolute;
+//   bottom: 1rem;
+// }
+// .Sidebar .SignOut button {
+//   outline: none;
+// }
+// .Sidebar .SignOut button i.fa {
+//   font-size: 2rem;
+// }
+}
 
 export default Sidebar;

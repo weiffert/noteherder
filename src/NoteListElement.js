7f39302e202a381fba653831b227ee80949a7b5f
weiffert <weiffert@gmail.com>
2018-05-22 15:17:45 -0400

    Refactored NoteListElement JSX to proper file

--- src/NoteListElement.js
+++ src/NoteListElement.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React from "react";
 
 const NoteListElement = () => {
   return (
-
+    <a className="active">
+      <li>
+        <div className="note">
+          <div className="note-title">Kohlrabi welsh</div>
+          <div className="note-body">
+            <p>
+              Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi
+              welsh onion daikon amaranth tatsoi tomatillo melon azuki bean
+              garlic.
+            </p>
+          </div>
+        </div>
+      </li>
+    </a>
   );
 };
 

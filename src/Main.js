6ebe8e1606da92c0df893c1beadc9d147f478fcd
weiffert <weiffert@gmail.com>
2018-05-22 17:43:05 -0400

    Refactored data into main

--- src/Main.js
+++ src/Main.js
@@ -4,20 +4,77 @@ import Sidebar from "./Sidebar";
 import NoteList from "./NoteList";
 import NoteForm from "./NoteForm";
 
-const Main = () => {
-  return (
-    <div className="Main" style={style}>
-      <Sidebar />
-      <NoteList />
-      <NoteForm />
-    </div>
-  );
-};
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [
+        {
+          key: 1,
+          active: false,
+          name: "Kohlrabi welsh",
+          body:
+            "Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic."
+        },
+        {
+          key: 2,
+          active: false,
+          name: "Dandelion cucumber",
+          body:
+            "Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini."
+        },
+        {
+          key: 3,
+          active: false,
+          name: "Prairie turnip",
+          body:
+            "Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip."
+        }
+      ]
+    };
+  }
+
+  handleMouseEnter(key) {
+    const notes = [...this.state.notes];
+    const index = notes.findIndex(note => note.key === key);
+    notes[index].active = true;
+    this.setState({
+      notes
+    });
+  }
+
+  handleMouseLeave(key) {
+    const notes = [...this.state.notes];
+    const index = notes.findIndex(note => note.key === key);
+    notes[index].active = false;
+    this.setState({
+      notes
+    });
+  }
+
+  handleClick(key) {
+    console.log(key);
+  }
+  render() {
+    return (
+      <div className="Main" style={style}>
+        <Sidebar />
+        <NoteList
+          onClick={key => this.handleClick(key)}
+          onMouseEnter={key => this.handleMouseEnter(key)}
+          onMouseLeave={key => this.handleMouseLeave(key)}
+          notes={this.state.notes}
+        />
+        <NoteForm />
+      </div>
+    );
+  }
+}
 
 const style = {
   display: "flex",
   height: "100vh",
-  alignItems: "stretch",
-}
+  alignItems: "stretch"
+};
 
 export default Main;
--- src/NoteList.js
+++ src/NoteList.js
@@ -3,69 +3,17 @@ import React from "react";
 import NoteListElement from "./NoteListElement";
 
 class NoteList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      notes: [
-        {
-          key: 1,
-          active: false,
-          name: "Kohlrabi welsh",
-          body:
-            "Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic."
-        },
-        {
-          key: 2,
-          active: false,
-          name: "Dandelion cucumber",
-          body:
-            "Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini."
-        },
-        {
-          key: 3,
-          active: false,
-          name: "Prairie turnip",
-          body:
-            "Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip."
-        }
-      ]
-    };
-  }
-
-  handleMouseEnter(key) {
-    console.log(key);
-    const notes = [...this.state.notes];
-    const index = notes.findIndex(note => note.key === key);
-    notes[index].active = true;
-    this.setState({
-      notes
-    });
-  }
-
-  handleMouseLeave(key) {
-    const notes = [...this.state.notes];
-    const index = notes.findIndex(note => note.key === key);
-    notes[index].active = false;
-    this.setState({
-      notes
-    });
-  }
-
-  handleClick(key) {
-    console.log(key);
-  }
-
   renderList() {
-    return this.state.notes.map((note, index) => {
+    return this.props.notes.map((note, index) => {
       return (
         <NoteListElement
           active={note.active}
           name={note.name}
           body={note.body}
           key={note.key}
-          onClick={this.handleClick.bind(this, note.key)}
-          onMouseEnter={this.handleMouseEnter.bind(this, note.key)}
-          onMouseLeave={this.handleMouseLeave.bind(this, note.key)}
+          onClick={() => this.props.onClick(note.key)}
+          onMouseEnter={() => this.props.onMouseEnter(note.key)}
+          onMouseLeave={() => this.props.onMouseLeave(note.key)}
         />
       );
     });

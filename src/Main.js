1325d5434f47958ebdc2be68e12dacc0ae9df485
weiffert <weiffert@gmail.com>
2018-05-22 11:26:09 -0400

    Styled Main using style object

--- src/Main.js
+++ src/Main.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React from "react";
 
-import Sidebar from './Sidebar';
-import NoteList from './NoteList';
-import NoteForm from './NoteForm';
+import Sidebar from "./Sidebar";
+import NoteList from "./NoteList";
+import NoteForm from "./NoteForm";
 
 const Main = () => {
   return (
-      <div className="Main">
-        <Sidebar />
-        <NoteList />
-        <NoteForm />
-      </div>
-    );
-  }
+    <div className="Main" style={style}>
+      <Sidebar />
+      <NoteList />
+      <NoteForm />
+    </div>
+  );
+};
+
+const style = {
+  display: "flex",
+  height: "100vh",
+  alignItems: "stretch",
+}
 
 export default Main;

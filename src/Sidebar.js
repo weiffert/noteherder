e4ffcddf29f8d796b05ca746f19d360c05fec419
weiffert <weiffert@gmail.com>
2018-05-22 13:18:25 -0400

    Convert styles

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -12,13 +12,13 @@ const Sidebar = () => {
       </div>
 
       <a href="/notes">
-        <img src={newHover} alt="New note" />
-        <img src={newIcon} alt="New note" />
+        <img src={newHover} alt="New note" style={styles.aImage} />
+        <img src={newIcon} alt="New note" style={styles.aImage} />
       </a>
 
-      <div className="SignOut">
-        <button>
-          <i className="fa fa-sign-out" />
+      <div className="SignOut" style={styles.SignOut}>
+        <button style={styles.SignOutButton}>
+          <i className="fa fa-sign-out" style={styles.i} />
         </button>
       </div>
     </div>
@@ -26,56 +26,53 @@ const Sidebar = () => {
 };
 
 const styles = {
-
-sidebar: {
-  width: "6rem",
-  backgroundColor: "#f3f3f3",
-  padding: "0.5rem 0", 
-  display: "flex", 
-  flexDirection: "column",
-  alignItems: "center",
-},
-logo: {
-  fontFamily: "Fauna One",
-  color: "#666",
-  fontSize: "3rem",
-},
-logoImg: {
-  width: "3rem",
-  paddingLeft: "0.5rem",
-},
-
-// .Sidebar a.new-note {
-//   margin-top: 2rem;
-//   position: relative;
-//   width: 40px;
-// }
-// .Sidebar button {
-//   background-color: transparent;
-//   border: 0;
-//   color: #008BF8;
-//   cursor: pointer;
-// }
-// .Sidebar a img {
-//   position: absolute;
-//   left: 0;
-//   width: 100%;
-//   transition: opacity 0.25s ease-in-out;
-// }
-// .Sidebar a:hover img.outline {
-//   opacity: 0;
-// }
-
-// .Sidebar .SignOut {
-//   position: absolute;
-//   bottom: 1rem;
-// }
-// .Sidebar .SignOut button {
-//   outline: none;
-// }
-// .Sidebar .SignOut button i.fa {
-//   font-size: 2rem;
-// }
-}
+  sidebar: {
+    width: "6rem",
+    backgroundColor: "#f3f3f3",
+    padding: "0.5rem 0",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  logo: {
+    fontFamily: "Fauna One",
+    color: "#666",
+    fontSize: "3rem"
+  },
+  logoImg: {
+    width: "3rem",
+    paddingLeft: "0.5rem"
+  },
+  newNote: {
+    marginTop: "2rem",
+    position: "relative",
+    width: "4rem"
+  },
+  button: {
+    backgroundColor: "transparent",
+    border: "0",
+    color: "#008bf8",
+    cursor: "pointer"
+  },
+  aImage: {
+    position: "absolute",
+    left: "0",
+    width: "100%",
+    transition: "opacity 0.25s ease-in-out"
+  },
+  aHoverImage: {
+    opacity: "0"
+  },
+  SignOut: {
+    position: "absolute",
+    bottom: "1rem"
+  },
+  SignOutButton: {
+    outline: "none"
+  },
+  i: {
+    fontSize: "2rem"
+  }
+};
 
 export default Sidebar;

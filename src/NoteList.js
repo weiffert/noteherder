819197bc2cb7f878909b5949a048c1e7986c6fd1
weiffert <weiffert@gmail.com>
2018-05-22 17:07:29 -0400

    Handle mouse events

--- src/NoteList.js
+++ src/NoteList.js
@@ -6,11 +6,34 @@ class NoteList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      notes: []
+      notes: [
+        {
+          key: 1,
+          active: false,
+          name: "Kohlrabi welsh",
+          body:
+            "Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic."
+        },
+        {
+          key: 2,
+          active: false,
+          name: "Dandelion cucumber",
+          body:
+            "Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini."
+        },
+        {
+          key: 3,
+          active: false,
+          name: "Prairie turnip",
+          body:
+            "Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip."
+        }
+      ]
     };
   }
 
   handleMouseEnter(key) {
+    console.log(key);
     const notes = [...this.state.notes];
     const index = notes.findIndex(note => note.key === key);
     notes[index].active = true;
@@ -28,16 +51,21 @@ class NoteList extends React.Component {
     });
   }
 
+  handleClick(key) {
+    console.log(key);
+  }
+
   renderList() {
-    this.state.notes.map((note, index) => {
+    return this.state.notes.map((note, index) => {
       return (
         <NoteListElement
           active={note.active}
           name={note.name}
           body={note.body}
-          key={index}
-          onMouseEnter={this.handleMouseEnter.bind(this, index)}
-          onMouseLeave={this.handleMouseLeave.bind(this, index)}
+          key={note.key}
+          onClick={this.handleClick.bind(this, note.key)}
+          onMouseEnter={this.handleMouseEnter.bind(this, note.key)}
+          onMouseLeave={this.handleMouseLeave.bind(this, note.key)}
         />
       );
     });
@@ -47,7 +75,9 @@ class NoteList extends React.Component {
     return (
       <div className="NoteList" style={styles.NoteList}>
         <h3 style={styles.h3}>Notes</h3>
-        <ul id="notes" style={styles.notes}>{this.renderList()}</ul>
+        <ul id="notes" style={styles.notes}>
+          {this.renderList()}
+        </ul>
       </div>
     );
   }

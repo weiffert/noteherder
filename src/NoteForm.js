8867b3e2ce180d288b0e0f83d7d40c2934c287f6
weiffert <weiffert@gmail.com>
2018-05-22 17:06:42 -0400

    Style form

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -2,10 +2,76 @@ import React from "react";
 
 const NoteForm = () => {
   return (
-    <div className="NoteForm">
-      NoteForm
+    <div className="NoteForm" style={styles.NoteForm}>
+      <div className="form-actions" style={styles.formActions}>
+        <button type="button" style={styles.button}>
+          <i className="fa fa-trash-o" style={styles.i} />
+        </button>
+      </div>
+      <form style={styles.form}>
+        <p>
+          <input
+            type="text"
+            name="title"
+            placeholder="Title your note"
+            style={styles.input}
+          />
+        </p>
+        <textarea name="body" style={styles.textarea} />
+      </form>
     </div>
   );
+};
+
+const styles = {
+  NoteForm: {
+    // -ms-flex-positive: '1',
+    flexGrow: "1",
+    padding: "0 3rem"
+  },
+  formActions: {
+    paddingTop: "1rem",
+    marginLeft: "-2rem",
+    display: "flex",
+    // -ms-flex-line-pack: 'center',
+    alignContent: "center"
+  },
+  button: {
+    border: "none",
+    background: "none",
+    padding: "0"
+  },
+  buttonSubmit: {
+    backgroundColor: "#008bf8",
+    borderRadius: "4px",
+    color: "#fff",
+    fontSize: "1.4rem",
+    padding: "1rem"
+  },
+  i: {
+    color: "#999",
+    fontSize: "2rem",
+    margin: "0"
+  },
+  form: {
+    margin: "0 auto",
+    maxWidth: "80rem"
+  },
+  input: {
+    border: "none",
+    fontSize: "200%",
+    fontFamily: "Fauna One",
+    color: "#008bf8",
+    fontWeight: "400",
+    width: "100%",
+    outline: "none"
+  },
+  textarea: {
+    borderColor: "#eee",
+    width: "100%",
+    height: "calc(100vh - 140px)",
+    fontSize: "1.3em"
+  }
 };
 
 export default NoteForm;

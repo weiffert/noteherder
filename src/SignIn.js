de22b707e0f3b900886b08c92ff67ff462efc92f
weiffert <weiffert@gmail.com>
2018-05-24 14:38:55 -0400

    Support multiple authentications

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,10 +1,21 @@
 import React from "react";
 
+import './SignIn.css';
+
+import {auth, githubProvider, googleProvider } from './base';
+
+import googleLogo from './google.svg';
+import quill from './quill.svg';
+
 const SignIn = ({ handleAuth }) => {
+  const authenticate = (provider) => {
+    auth.signInWithPopup(provider);
+  };
+
   return (
     <div className="SignIn">
       <header className="Header">
-        <img src="media/quill.svg" alt="" />
+        <img src="" alt="" />
         <span className="title">Noteherder</span>
       </header>
       <main>
@@ -13,12 +24,12 @@ const SignIn = ({ handleAuth }) => {
           You never know when you'll need to write crap down. In fact, you
           should probably be taking notes right now.
         </p>
-        <button className="github" onClick={handleAuth}>
-          <i className="fa fa-github" />
+        <button className="github" onClick={() => authenticate(githubProvider)}>
+          <i className="fab fa-github" />
           Sign in with GitHub
         </button>
-        <button className="google">
-          <img src="media/google.svg" alt="" />
+        <button className="google" onClick={() => authenticate(googleProvider)}>
+          <img src={googleLogo} alt="" />
           Sign in with Google
         </button>
       </main>

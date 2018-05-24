574a4a263b32fa5dcfd845ef2db67a28beceb94b
weiffert <weiffert@gmail.com>
2018-05-24 14:38:40 -0400

    Handle multiple users

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 import "./App.css";
-
+import { auth } from './base';
 import Main from "./Main";
 import SignIn from "./SignIn";
 
@@ -10,8 +10,24 @@ class App extends Component {
     uid: null
   };
 
-  handleAuth = () => {
-    this.setState({ uid: "weiffert" });
+  componentWillMount() {
+    const login = window.localStorage.getItem('login');
+    if(login) {
+      this.handleAuth(login);
+    }
+    
+    auth.onAuthStateChanged(user => {
+      if(user) {
+        this.handleAuth(user.uid);
+      } else {
+        this.signOut();
+      }
+    });
+  }
+
+  handleAuth = (user) => {
+    this.setState({user});
+    window.localStorage.setItem('login', this.state.user);
   };
 
   signedIn = () => {
@@ -19,14 +35,16 @@ class App extends Component {
   };
 
   signOut = () => {
+    window.localStorage.setItem('login', "");
     this.setState({ uid: null });
+    auth.signOut();
   };
 
   render() {
     return (
       <div className="App">
         {this.signedIn() ? (
-          <Main signOut={this.signOut} />
+          <Main signOut={this.signOut} user={this.state.uid} />
         ) : (
           <SignIn handleAuth={this.handleAuth} />
         )}
--- src/Main.js
+++ src/Main.js
@@ -15,7 +15,7 @@ class Main extends React.Component {
   }
 
   componentWillMount() {
-    base.syncState("notes", {
+    base.syncState(`${this.props.user}-notes`, {
       context: this,
       state: "notes",
       asArray: true
